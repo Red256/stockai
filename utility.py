e3ac6031e6ab98b569df04dcaf1443c55a46998a
stempro <stemproaca@gmail.com>
2023-07-17 08:14:18 -0700

    add new req

--- utility.py
+++ utility.py
@@ -1,14 +1,15 @@
+import  pandas as pd 
 
 class DataProcessing():
     def __init__(self):
-        pass 
-    def clearning(self, df, columns=['Ticker', 'Unnamed: 0', 'Repaired?']):
+        pass
+    def cleaning(self, df, columns=['Ticker', 'Unnamed: 0', 'Repaired?']):
         """
             remove columns
         """
-        cols = [col for col in columns if col in list(df)] 
+        cols = [col for col in columns if col in list(df)]
         if cols:
             df.drop(columns=cols, inplace=True)
             print("things are")
-            return df 
-        return df 
+            return df
+        return df
